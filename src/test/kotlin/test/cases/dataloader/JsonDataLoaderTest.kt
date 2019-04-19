75f87840df5890bfcfab2a34dad7ffc870be186e
Dane Wicki <wicki.dane@gmail.com>
2019-04-19 13:45:45 +0200

    Implement a multiple testData loader

--- src/test/kotlin/test/cases/dataloader/JsonDataLoaderTest.kt
+++ src/test/kotlin/test/cases/dataloader/JsonDataLoaderTest.kt
@@ -4,12 +4,23 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import util.ReadableFile
 import util.ReadableFileStub
+
 internal class JsonDataLoaderTest {
 
     @Test
     fun loadTestData() {
-        var readableFile: ReadableFile = ReadableFileStub("{id:1,name:'test',preParameters:['Test'],postParameters:['test2'],testType:'A'}")
+        var readableFile: ReadableFile =
+            ReadableFileStub("{id:1,name:'test',preParameters:['Test'],postParameters:['test2'],testType:'A'}")
         var testee = JsonDataLoader(readableFile)
-        assertEquals(TestData(1,"test",listOf("Test"),listOf("test2"),TestType.A), testee.loadTestData())
+        assertEquals(TestData(1, "test", listOf("Test"), listOf("test2"), TestType.A), testee.loadSingleTestData())
+    }
+
+    @Test
+    fun loadMultipleTestData() {
+        val singleTest = "{ id:1, name:'test', preParameters:['Test'], postParameters:['test2'], testType:'A' }"
+        val testData = TestData(1, "test", listOf("Test"), listOf("test2"), TestType.A)
+        var readableFile: ReadableFile = ReadableFileStub("[$singleTest, $singleTest]")
+        var testee = JsonDataLoader(readableFile)
+        assertEquals(mutableListOf(testData, testData), testee.loadMultipleTestData())
     }
 }
