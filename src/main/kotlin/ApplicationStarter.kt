456a1c33dce5aaf0aacb1302f812dc702ae9b32f
KilnOfTheSecondFlame <pbauma@gmx.net>
2019-04-19 18:01:51 +0200

    Move reading from resources folder to separate util class

--- src/main/kotlin/ApplicationStarter.kt
+++ src/main/kotlin/ApplicationStarter.kt
@@ -2,15 +2,14 @@ import feature.report.ReportTxtPersistor
 import gui.GuiLess
 import gui.ScannerAbstractionImpl
 import test.cases.dataloader.JsonDataLoader
-import util.ReadableFileImpl
+import util.ReadableResourceFile
 import util.WriteableFileImpl
 import java.io.File
 
 fun main() {
     val scannerAbstractionImpl = ScannerAbstractionImpl()
     val guiLess = GuiLess(scannerAbstractionImpl)
-    val jsonFile = File(MultipleSequentialTestExecutor::class.java.getResource("package.json").path)
-    val jsonReadableFile = ReadableFileImpl(jsonFile)
+    val jsonReadableFile = ReadableResourceFile("package.json")
     val dataLoader = JsonDataLoader(jsonReadableFile)
     val txtFile = File("./Report.txt")
     val reportPersistor = ReportTxtPersistor(WriteableFileImpl(txtFile))
---
+++ src/main/kotlin/util/ReadableResourceFile.kt
@@ -0,0 +1,19 @@
+package util
+
+import java.io.File
+import java.io.FileNotFoundException
+
+class ReadableResourceFile(resourceName: String) : ReadableFile {
+    private var file: File
+
+    init {
+        val path: String = ReadableResourceFile::class.java.getResource("../$resourceName").path
+        System.out.println(path)
+        file = File(path)
+        if (!file.exists() && !file.canRead()) throw FileNotFoundException()
+    }
+
+    override fun getText(): String {
+        return file.inputStream().bufferedReader().readText()
+    }
+}
