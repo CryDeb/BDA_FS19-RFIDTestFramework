84c1e9a1114c3f0e915ed2c93e6bad973f63ce97
KilnOfTheSecondFlame <pbauma@gmx.net>
2019-04-20 15:20:27 +0200

    Fix tests that broke 😅

--- src/test/kotlin/test2/TestCaseRunnerFactoryTest.kt
+++ src/test/kotlin/test2/TestCaseRunnerFactoryTest.kt
@@ -6,22 +6,22 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import test.cases.TestCaseRunnerFactory
 import test.cases.dataloader.TestType
-import test.cases.runner.SingleTagMultipleAntennas
 import test.cases.runner.MultipleTagsSingleRead
+import test.cases.runner.SingleTagMultipleAntennas
 
 internal class TestCaseRunnerFactoryTest {
 
     @Test
-    fun getRunnerByTypeB() {
+    fun getRunnerByTypeSingleTagMultipleAntennas() {
         val testee = TestCaseRunnerFactory()
-        val runnable: Runnable = testee.getRunnerByType(TestType.B)
+        val runnable: Runnable = testee.getRunnerByType(TestType.SingleTagMultipleAntennas)
         assertThat(runnable, CoreMatchers.instanceOf(SingleTagMultipleAntennas::class.java))
     }
 
     @Test
     fun getRunnerByTypeC() {
         val testee = TestCaseRunnerFactory()
-        val runnable: Runnable = testee.getRunnerByType(TestType.C)
+        val runnable: Runnable = testee.getRunnerByType(TestType.MultipleTagsSingleRead)
         assertThat(runnable, CoreMatchers.instanceOf(MultipleTagsSingleRead::class.java))
     }
 }
